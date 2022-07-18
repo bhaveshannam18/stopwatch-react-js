f2830c7b67b1631e70f72a29357b93f29fb8fe47
Bhavesh Annam <bhaveshannam18@gmail.com>
2022-07-18 22:29:13 +0530

    added usestate

--- src/Stopwatch/Stopwatch.js
+++ src/Stopwatch/Stopwatch.js
@@ -1,21 +1,58 @@
 import "./Stopwatch.css";
 import GenericSpan from "./Components/GenericSpan";
+import { useState } from "react";
 function Stopwatch(){
+    const [stopwatchState , setStopwatchState] = useState(0);
     return (
     <>
         <div className="container">
-            {/* <span className="startTimer">Start</span>
-            <span className="stopTimer">Stop</span>
-            <span className="timerValue">00:00:00</span>
-            {/* <span className="pauseTimer">Pause</span> */}
-            {/* <span className="resumeTimer">Resume</span> */} 
-            <GenericSpan 
+
+            {stopwatchState === 0 && (<GenericSpan 
             type="start" 
             text="Start" 
             handleClick={()=> {
-                console.log("Start");
+                setStopwatchState(1);
+            }}
+            />)}
+
+            {stopwatchState === 1 && (<><GenericSpan 
+            type="stop" 
+            text="Stop" 
+            handleClick={()=> {
+                setStopwatchState(0);
             }}
             />
+            <GenericSpan 
+            type="timer" 
+            text="00:00:00" 
+            />
+            <GenericSpan 
+            type="pause" 
+            text="Pause" 
+            handleClick={()=> {
+                setStopwatchState(2);
+            }}
+            />
+            </>)}
+            {stopwatchState === 2 && (<><GenericSpan 
+            type="stop" 
+            text="Stop" 
+            handleClick={()=> {
+                setStopwatchState(0);
+            }}
+            />
+            <GenericSpan 
+            type="timer" 
+            text="00:00:00" 
+            />
+            <GenericSpan 
+            type="resume" 
+            text="Resume" 
+            handleClick={()=> {
+                setStopwatchState(1);
+            }}
+            /></>)}
+
         </div>        
     </>
     );
