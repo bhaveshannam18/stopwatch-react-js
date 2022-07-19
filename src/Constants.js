47b891f6c33e32ef0d55f702aa55ed2a87eaf206
Bhavesh Annam <bhaveshannam18@gmail.com>
2022-07-19 23:22:39 +0530

    start , stop , pause , resume seconds

---
+++ src/Constants.js
@@ -0,0 +1,28 @@
+export const Start = {
+    type:"start",
+    text:"Start",
+    className:"startTimer"
+};
+
+export const Stop = {
+    type:"stop",
+    text:"Stop",
+    className:"stopTimer"
+};
+
+export const Timer = {
+    type:"timer",
+    className:"TimerValue"
+};
+
+export const Pause = {
+    type:"pause",
+    text:"Pause",
+    className:"pauseTimer"
+};
+
+export const Resume = {
+    type:"resume",
+    text:"Resume",
+    className:"resumeTimer"
+};
--- src/Stopwatch/Stopwatch.js
+++ src/Stopwatch/Stopwatch.js
@@ -1,54 +1,89 @@
 import "./Stopwatch.css";
 import GenericSpan from "./Components/GenericSpan";
+import {Start , Stop , Pause , Resume , Timer} from "../Constants";
 import { useState } from "react";
 function Stopwatch(){
     const [stopwatchState , setStopwatchState] = useState(0);
+    const [secPassed , setSecPassed] = useState(0);
+    const [timerId , setTimerId] = useState(null);
+
+    const startTimer = (_) => {
+        let tId = setInterval(()=>{
+            setSecPassed((sec)=>sec=sec+1);
+        },1000);
+        setTimerId(tId);
+    }
+
+    const stopTimer = () =>{
+        clearInterval(timerId);
+        setSecPassed(0);
+    }
+
+    const pauseTimer = ()=>{
+        clearInterval(timerId);
+    }
+
+    const resumeTimer = ()=>{
+        startTimer();
+    }
     return (
     <>
         <div className="container">
 
             {stopwatchState === 0 && (<GenericSpan 
-            type="start" 
-            text="Start" 
+            type={Start.type} 
+            text={Start.text}
+            className={Start.className} 
             handleClick={()=> {
+                startTimer();
                 setStopwatchState(1);
             }}
             />)}
 
             {stopwatchState === 1 && (<><GenericSpan 
-            type="stop" 
-            text="Stop" 
+            type={Stop.type} 
+            text={Stop.text}
+            className={Stop.className}
             handleClick={()=> {
+                stopTimer();
                 setStopwatchState(0);
             }}
             />
             <GenericSpan 
-            type="timer" 
-            text="00:00:00" 
+            type={Timer.type}
+            className={Timer.className}
+            text={secPassed} 
             />
             <GenericSpan 
-            type="pause" 
-            text="Pause" 
+            type={Pause.type} 
+            text={Pause.text}
+            className={Pause.className}
             handleClick={()=> {
+                pauseTimer();
                 setStopwatchState(2);
             }}
             />
             </>)}
             {stopwatchState === 2 && (<><GenericSpan 
-            type="stop" 
-            text="Stop" 
+            type={Stop.type} 
+            text={Stop.text}
+            className={Stop.className}
             handleClick={()=> {
+                stopTimer();
                 setStopwatchState(0);
             }}
             />
             <GenericSpan 
-            type="timer" 
-            text="00:00:00" 
+            type={Timer.type}
+            className={Timer.className}
+            text={secPassed}
             />
             <GenericSpan 
-            type="resume" 
-            text="Resume" 
+            type={Resume.type} 
+            text={Resume.text}
+            className={Resume.className}
             handleClick={()=> {
+                resumeTimer();
                 setStopwatchState(1);
             }}
             /></>)}
